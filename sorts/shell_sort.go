ba26d01ff1b5d43ccb6ab5f66ec64be66f452b8c
ayan0312 <ayan0312000@outlook.com>
2020-11-05 23:03:35 +0800

    shell sort

---
+++ sorts/shell_sort.go
@@ -0,0 +1,22 @@
+package sorts
+
+func ShellSort(arr []int) {
+	increment := len(arr) / 2
+	for increment > 0 {
+		for i := increment; i < len(arr); i++ {
+			j := i
+			temp := arr[i]
+
+			for j >= increment && arr[j-increment] > temp {
+				arr[j] = arr[j-increment]
+				j -= increment
+			}
+			arr[j] = temp
+		}
+		if increment == 2 {
+			increment = 1
+		} else {
+			increment = int(increment * 5 / 11)
+		}
+	}
+}
